544395d3c47d30eee0442c9c258eb14d6026f4e4
jordan wallace <jordanwallace644@gmail.com>
2021-10-23 20:43:29 -0500

    int

---
+++ app.js
@@ -0,0 +1,44 @@
+function refreshTime() {
+  let date = new Date(); //date object
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let datNight = "";
+
+  //am and dayNight
+
+  if (h == 0) {
+    h = 12;
+  }
+  if (h > 12) {
+    h -= 12;
+    dayNight = "pm";
+  } else {
+    dayNight = "am";
+  }
+  // add zero place holder infront off single digits
+  if (h < 10) {
+    h = "0" + h;
+  } else {
+    h = h;
+  }
+
+  if (m < 10) {
+    m = "0" + m;
+  } else {
+    m = m;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  } else {
+    s = s;
+  }
+
+  let time = `${h}:${m}:${s} ${dayNight} `;
+
+  document.getElementById("clock-nums").innerHTML = time;
+
+  setTimeout(refreshTime, 1000);
+}
+
+refreshTime();
